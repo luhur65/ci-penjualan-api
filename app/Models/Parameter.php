b4a6a3f050b6a15c77d4c5506c6ef6a4275bbc07
Dharma Situmorang <dharmabakti1202@gmail.com>
2026-04-07 14:02:27 +0700

    fix: make parameter modul

--- app/Models/Parameter.php
+++ app/Models/Parameter.php
@@ -2,17 +2,54 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
-
-class Parameter extends Model
+class Parameter extends CustomModel
 {
     protected $table            = 'parameters';
     protected $primaryKey       = 'id';
+    protected $sortDirection    = 'ASC';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'grp',
+        'subgrp', 
+        'kelompok',
+        'text',
+        'memo',
+        'type',
+        'is_default',
+        'modified_by',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $fieldMap = [
+        'grp' => 'parameters.grp',
+        'subgrp' => 'parameters.subgrp',
+        'kelompok' => 'parameters.kelompok',
+        'text' => 'parameters.text',
+        'memo' => 'parameters.memo',
+        'type' => 'parameters.type',
+        'is_default' => 'parameters.is_default',
+        'modifiedby' => 'parameters.modified_by',
+        'created_at' => 'parameters.created_at',
+        'updated_at' => 'parameters.updated_at'
+        
+    ];
+
+    protected $searchableFields = [
+        'grp',
+        'subgrp',
+        'kelompok',
+        'text',
+        'memo',
+        'type',
+        'is_default',
+        'modified_by',
+        'created_at',
+        'updated_at'
+    ];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
@@ -43,4 +80,34 @@ class Parameter extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getAll()
+    {
+        $query = $this->builder();
+        $query->select([
+            'parameters.grp',
+            'parameters.subgrp',
+            'parameters.kelompok',
+            'parameters.text',
+            'parameters.memo',
+            'parameters.type',
+            'parameters.is_default as default',
+            'parameters.modified_by as modifiedby',
+            'parameters.created_at',
+            'parameters.updated_at'
+        ]);
+
+        return $this->datatable($query);
+    }
+
+    public function findOne($id = null)
+    {
+        // TODO: mapping data
+        return $this->where('id', $id)->first();
+    }
 }
